563e9a779339524741d861496bc91429db6699f8
Malik Ahsan <malikahsan13@gmail.com>
2026-02-27 12:00:40 +0500

    file upload using Upload file

--- app/upload_file.py
+++ app/upload_file.py
@@ -9,4 +9,8 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 @app.post("/upload_file")
-async def upload_file()
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+    return JSONResponse(content={"message":"File uploaded successfully", "filename":file.filename, "content_type":file.content_type})
