8b35843e493935eab277268682f5f1229a77f3d0
Malik Ahsan <malikahsan13@gmail.com>
2026-03-01 14:05:14 +0500

    added fetch all users endpoint

--- app/crud_rest_api_mongodb.py
+++ app/crud_rest_api_mongodb.py
@@ -1,4 +1,4 @@
-from pymongo import mongo_client
+from pymongo import MongoClient
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from bson import ObjectId
@@ -6,7 +6,7 @@ from bson import ObjectId
 app = FastAPI()
 
 uri = "mongodb://localhost:27017/fastapi-db"
-client = mongo_client(uri)
+client = MongoClient(uri)
 database = client.get_databae()
 
 
@@ -33,3 +33,13 @@ def insert_user(user_data: User):
 async def create_user(user_data: User):
     user_id = insert_user(user_data)
     return user_id
+
+
+@app.get("/users/")
+async def get_all_users(db: MongoClient = Depends(get_current_database)):
+    users = users_collection.find()
+    user_list = []
+    for user in users:
+        user['_id'] = str(user['_id'])
+        user_list.append(user)
+    return user_list
