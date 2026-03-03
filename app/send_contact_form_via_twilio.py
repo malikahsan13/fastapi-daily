4415485162ceef67455d9812e75f10e0ab020e81
Malik Ahsan <malikahsan13@gmail.com>
2026-03-03 23:53:49 +0500

    logic for sending whatsapp message via twilio

--- app/send_contact_form_via_twilio.py
+++ app/send_contact_form_via_twilio.py
@@ -9,3 +9,15 @@ TWILIO_PHONE_NUMBER = ""
 RECIPIENT_WHATSAPP_NUMBER = ""
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
+@app.post("/send-whatsapp/")
+async def send_whatsapp(name: str= Form(...), email: str = Form(...), message: str = Form(...)):
+    try:
+        whatsapp_msg = f"New contact form submission:\nName: {name}\nEmail: {email}\nMessage:{message}"
+        message = client.message.create(
+            body:whatsapp_msg,
+            form = "whatsapp:"+TWILIO_PHONE_NUMBER,
+            to="whatsapp:"+RECIPIENT_WHATSAPP_NUMBER
+        )
+        return {"status":"success","message_sid":message.sid}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
