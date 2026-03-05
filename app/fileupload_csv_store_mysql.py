6d474a9a59ba83d4a8aa9dc2cd6f375bd25d71f5
Malik Ahsan <malikahsan13@gmail.com>
2026-03-06 01:15:25 +0500

    create table structure

--- app/fileupload_csv_store_mysql.py
+++ app/fileupload_csv_store_mysql.py
@@ -4,6 +4,23 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_RUL = "mysql+mysqlconnect://root:Admin123@localhost/mydb"
+DATABASE_URL = "mysql+mysqlconnect://root:Admin123@localhost/mydb"
 
 Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = 'User'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String,  index=True)
+    name = Column(String)
+
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+app = FastAPI()
+
+
+@app.post("/upload-csv/")
