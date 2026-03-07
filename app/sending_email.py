c562e40e7a84446618238cea9668fd53fc17a642
Malik Ahsan <malikahsan13@gmail.com>
2026-03-08 02:31:55 +0500

    added send email credentials in endpoint

--- app/sending_email.py
+++ app/sending_email.py
@@ -11,3 +11,15 @@ class EmailSchema(BaseModel):
     recipient_email: str
     subject: str
     body: str
+
+
+@app.post("/send_email/")
+async def send_email(email_data: EmailSchema):
+    sender_email = "abc@mail.com"
+    sender_password = "abc123"
+    recipient_email = "bcd@mail.com"
+    subject = email_data.subject
+    body = email_data.body
+
+    smtp_server = smtplib.SMTP("smtp@gmail.com", 587)
+    smtp_server.starttls()
