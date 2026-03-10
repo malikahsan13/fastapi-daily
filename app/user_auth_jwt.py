0620338bf0ebb4e11bd7697e88e2eb713faf1e8f
Malik Ahsan <malikahsan13@gmail.com>
2026-03-11 02:12:52 +0500

    added jwt settings an db connection method

--- app/user_auth_jwt.py
+++ app/user_auth_jwt.py
@@ -14,3 +14,20 @@ app = FastAPI()
 # Create all database tables
 Base.metadata.create_all(bind=engine)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# JWT authentication settings
+SECRET_KEY = "your-secret-key"
+ALGOROITHM = "H256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# Password hasing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated=auto)
+
+# Dependency to get database session
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
