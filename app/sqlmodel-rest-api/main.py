7609a0519ec17f2e82a4f4f9880619269cf6b527
Malik Ahsan <malikahsan13@gmail.com>
2026-03-13 01:37:36 +0500

    added create book and read all books endpoint

--- app/sqlmodel-rest-api/main.py
+++ app/sqlmodel-rest-api/main.py
@@ -4,3 +4,45 @@ from database import create_db_and_tables, engine
 from models import Book
 from contextlib import asynccontextmanager
 from typing import Optional, List
+
+# initialize the database and create tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Perform startup tasks
+    create_db_and_tables()
+
+    yield  # The app is now "running" and handling requests
+
+    # --- SHUTDOWN ---
+    # print("Disconnecting from Database...")
+    # db_connection.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+# Dependency to get a new database session per request
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# create new book
+
+
+@app.post("/books/", response_model=Book)
+def create_book(book: Book, session: Session = Depends(get_session)):
+    session.add(book)
+    session.commit()
+    # refresh to get the updated instance with generated id
+    session.refresh(book)
+    return book
+
+
+@app.get("/books/", response_mode=List[Book])
+def read_books(session: Session = Depends(get_session)):
+    statement = select(Book)
+    results = session.exec(statement)
+    return results.all()
