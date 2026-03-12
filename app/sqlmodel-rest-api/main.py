e7a25de952f70259b91b33e31bc8cba0c8c40ed4
Malik Ahsan <malikahsan13@gmail.com>
2026-03-13 01:43:36 +0500

    created update, get by book id and delete endpoint

--- app/sqlmodel-rest-api/main.py
+++ app/sqlmodel-rest-api/main.py
@@ -46,3 +46,38 @@ def read_books(session: Session = Depends(get_session)):
     statement = select(Book)
     results = session.exec(statement)
     return results.all()
+
+
+@app.get("/books/{book_id}", response_model=Book)
+def read_book(book_id: int, session: Session = Depends(get_session)):
+    book = session.get(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
+
+
+@app.put("/books/{book_id}", response_model=Book)
+def update_book(book_id: int, updated_book: Book, session: Session = Depends(get_session)):
+    book = session(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    book.title = updated_book.title
+    book.author = updated_book.author
+    book.description = updated_book.description
+    book.year = updated_book.year
+
+    session.add(book)
+    session.commit()
+    session.refresh(book)
+    return book
+
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int, session: Session = Depends(get_session)):
+    book = session(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    session.delete(book)
+    session.commit()
+    return {"ok": True}
