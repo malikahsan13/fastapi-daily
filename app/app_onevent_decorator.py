ad56aa4e2a5a827d50b186bfe30be29ed9120bbc
Malik Ahsan <malikahsan13@gmail.com>
2026-03-14 00:31:22 +0500

    usage of updated async context manager lifespan approach

--- app/app_onevent_decorator.py
+++ app/app_onevent_decorator.py
@@ -1,17 +1,33 @@
 # @app.on_event decorator is deprecated
 # use lifespan event handler instead
 
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.on_event("startup")
+# async def on_startup():
+#     # create_db_and_tables()
+#     print("App is starting up...")
+
+
+# @app.on_event("shutdown")
+# async def on_shutdown():
+#     print("App is shutting down")
+
+
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def on_startup():
-    # create_db_and_tables()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     print("App is starting up...")
+    yield
+    print("App is shutting down...")
 
 
-@app.on_event("shutdown")
-async def on_shutdown():
-    print("App is shutting down")
+app = FastAPI(lifespan=lifespan)
+
+# yields control back to the application in between startup and shutdown
