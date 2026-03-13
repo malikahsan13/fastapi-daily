80aad0b4f1d60f90ffc5bd7ba81884d3b3149b27
Malik Ahsan <malikahsan13@gmail.com>
2026-03-14 01:03:55 +0500

    created endpoints for fetching data of one to many relation

--- app/one_to_many_sqlmodel/testing.py
+++ app/one_to_many_sqlmodel/testing.py
@@ -30,3 +30,34 @@ app = FastAPI(lifespan=lifespan)
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+@app.post("/authors/")
+def create_author(author: Author, session: Session = Depends(get_session)):
+    author = Author(name="J.K. Rowling", book=[Book(title="Harry Potter and the Sorcerer's Stonr", year=1997), Book(
+        title="Happy Potter and the Chamber of Secrets", year=1998)])
+    session.add(author)
+    session.commit()
+    session.refresh(author)
+    return author
+
+
+@app.get("/authors/", response_model=List[AuthorRead])
+def read_authors(session: Session = Depends(get_session)):
+    authors = session.exec(
+        select(Author).options(selectinload(Author.books))
+    ).all()
+    return authors
+
+
+def insert_data():
+    with Session(engine) as session:
+        author = Author(name="J.K. Rowling", books=[
+            Book(title="Happy Potter and the Sorcerer's Stone", year=1997),
+            Book(title="Happy Potter and the Chamber of Secrets", year=1998)
+        ])
+        session.add(author)
+        session.commit()
+
+
+insert_data()
