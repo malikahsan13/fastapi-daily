7ce60ada56c488e2dcec0a4bac2df6ba9f1000f7
Malik Ahsan <malikahsan13@gmail.com>
2026-03-16 01:16:27 +0500

    created database connection file for many to many relation

---
+++ app/many_to_many_sqlmodel/database.py
@@ -0,0 +1,15 @@
+from sqlmodel import SQLModel, create_engine
+
+mysql_username = "root"
+mysql_password = "123456"
+mysql_host = "localhost"
+mysql_port = "3306"
+mysql_database = "testdb"
+
+mysql_url = f"mysql+mysqlconnector://{mysql_username}:{mysql_password}@{mysql_host}{mysql_port}/{mysql_database}"
+
+engine = create_engine(mysql_url, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
